1e112ec631ca382e9e9bcb2b4a1c1cb73a681520
Nikola Jolovic <nikolajolovic02@gmail.com>
2026-05-01 18:39:58 +0200

    [feat] backend bootstrap

---
+++ server/src/main/java/com/github/nikola352/chess_endgame_analyzer/ChessEndgameAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.github.nikola352.chess_endgame_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEndgameAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessEndgameAnalyzerApplication.class, args);
+	}
+
+}
